5c63b5cad421e0360f68543c5475e7cdf5442c9e
Erica Liu <ericaliu@mit.edu>
2018-04-17 06:51:04 -0400

    progress bar for todo list

---
+++ calendar.js
@@ -0,0 +1,24 @@
+function setProgress() {
+  var numberOfChecks = 0;
+  var inputs = document.getElementsByTagName("input");
+  var numberOfBoxes = 0;
+
+  for (var i = 0; i < inputs.length; i++) {
+    if (inputs[i].type === "checkbox") {
+      if (!inputs[i].hidden) numberOfBoxes++;
+      if (inputs[i].checked) numberOfChecks++;
+    }
+
+  }
+
+  console.log(numberOfBoxes);
+  console.log(numberOfChecks);
+
+  if (numberOfChecks == 0) {
+    document.getElementById("progress").src = "empty.png";
+  } else if (numberOfChecks == numberOfBoxes) {
+    document.getElementById("progress").src = "full.png";
+  } else {
+    document.getElementById("progress").src = "middle.png";
+  }
+}
