a3491c6f6fc4e0dcc2c5f4b2169855fd201f37f4
Katherine Wang <kwang@dhcp-18-111-119-11.dyn.MIT.EDU>
2018-04-26 22:40:33 -0400

    injuries can now be saved and added dynamically

--- assets/information.js
+++ assets/information.js
@@ -1,27 +1,80 @@
+var injuries = ["Knee Dislocation"];
+// for more permanent do localStorage
+if (sessionStorage.injuries) {
+    injuries = JSON.parse(sessionStorage.getItem("injuries"));
+} else {
+    sessionStorage.setItem("injuries", JSON.stringify(injuries));
+}
+
+function setupInjuries(){
+    var dropdown = document.getElementById("injuryDropdown");
+    for (i = 0; i < injuries.length; i++){
+
+      var inj= document.createElement('p');
+      var id = "injury" + i.toString();
+      inj.id = id;
+      inj.innerHTML = injuries[i];
+      inj.onclick = function (num) {
+            return function () {
+                setInjury(num);
+            };
+        }(id);
+      dropdown.appendChild(inj);
+      }
+      var iadd= document.createElement('button');
+      iadd.id = "iadd";
+      iadd.innerHTML = "&#x2b;"
+      iadd.onclick = function() {
+        document.getElementById('injuryModal').style.display = "block";
+      }
+      dropdown.appendChild(iadd);
+}
+
+function setUpModal(){
+  var modal = document.getElementById('injuryModal');
+  var span = document.getElementsByClassName("close")[0]; 
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+      modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+  }
+}
+
+
 function selectInjury() {
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("injuryDropdown").classList.toggle("show");
 }
 
 function setInjury(injury){
     document.getElementById("dropbtn").innerHTML =  document.getElementById(injury).innerHTML + '<i class="fa fa-angle-double-down"></i>';
 }
 
-/*Util.events(document, {
-  // Final initalization entry point: the Javascript code inside this block
-  // runs at the end of start-up when the DOM is ready
-  "DOMContentLoaded": function() { 
-
-
-    // Adds click listeners to all the buttons in the interface
-    //TODO: make more dynamic
-    document.querySelector("#injury1").addEventListener("click", function(evt) {
-      setInjury("Knee Dislocation");
-    });
-    document.querySelector("#injury2").addEventListener("click", function(evt) {
-      setInjury("Elbow Dislocation");
-    });
-  },
-});*/
+function addInjury(){
+    var newInjury = document.getElementById("newInjury").value;
+    if(newInjury != ""){
+        var dropdown = document.getElementById("injuryDropdown");
+        var inj= document.createElement('p');
+        var id = "injury" + injuries.length.toString();
+        inj.id = id;
+        inj.innerHTML = newInjury;
+        inj.onclick = function (num) {
+            return function () {
+                setInjury(num);
+            };
+        }(id);
+      dropdown.insertBefore(inj, dropdown.childNodes[injuries.length + 1]);
+      injuries.push(newInjury);
+      sessionStorage.setItem("injuries", JSON.stringify(injuries));
+    }
+    document.getElementById('injuryModal').style.display = "none";
+}
 
 
 // Close the dropdown menu if the user clicks outside of it
---
+++ assets/profile.js
@@ -0,0 +1,17 @@
+var injuries = ["Knee Dislocation"];
+// for more permanent do localStorage
+if (sessionStorage.injuries) {
+    injuries = JSON.parse(sessionStorage.getItem("injuries"));
+} else {
+    sessionStorage.setItem("injuries", JSON.stringify(injuries));
+}
+
+var INJURY_DESCRIPTOR = "<strong>Area(s) of injury:</strong>"
+
+function setUpProfileInjuries(){
+	var injuryString = INJURY_DESCRIPTOR + "<br>";
+	for (i = 0; i < injuries.length; i++){
+		injuryString += injuries[i] + "<br>"
+	}
+	document.getElementById("injuryarea").innerHTML = injuryString;
+}
