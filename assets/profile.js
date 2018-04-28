1eb28e3e669c89187207ea5bd0b456dca80858c4
Katherine Wang <kwang@dhcp-18-111-120-234.dyn.mit.edu>
2018-04-28 09:38:12 -0400

    More modal closing and adding modal for profile

--- assets/profile.js
+++ assets/profile.js
@@ -34,3 +34,24 @@ function viewMessages(){
 	document.getElementById("messages").innerHTML = messageString;
 
 }
+
+function openModal(){
+    document.getElementById('profileModal').style.display = "block";
+}
+
+function setUpModal(){
+  var modal = document.getElementById('profileModal');
+  var span = document.getElementsByClassName("close")[0]; 
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+      modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+  }
+}
